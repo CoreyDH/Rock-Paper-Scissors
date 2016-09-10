20644f78ce110c6476993f357e33be75e67b048b
Corey H <rewer6@gmail.com>
2016-09-10 15:11:13 -0500

    adfasd

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -18,21 +18,15 @@
     var local = [];
     var dbLength = 0;
 
-    // playerDB.on('value', function(snapshot) {
-    //
-    //   var dataObj = snapshot.val();
-    //
-    //   if(dataObj) {
-    //     // if(playerArrKeys.length !== Object.keys(dataObj).length) {
-    //     //   playerArrKeys = Object.keys(dataObj);
-    //     // }
-    //     dbLength = Object.keys(dataObj).length;
-    //     if(Object.keys(dataObj).length === 2) {
-    //       $('.rps-join').hide();
-    //     }
-    //   }
-    //
-    // });
+    playerDB.on('child_changed', function(snapshot) {
+
+      getFBArrayLength(function(len) {
+        if(len === numPlayers) {
+          $('.rps-join').hide();
+        }
+      });
+
+    });
 
     playerDB.on('child_added', function(snapshot) {
 
@@ -46,7 +40,7 @@
     });
 
     playerDB.on('child_removed', function(snapshot) {
-      console.log(snapshot);
+      console.log(snapshot.val());
       var player = snapshot.val();
       vacateSeat(player.id);
 
@@ -54,17 +48,34 @@
       console.log('error '+ e);
     });
 
+    $('.rps-join-game').on('click', function() {
+
+      var name = $('#rps-name').val().trim();
+
+      if(name) {
+        getFBArrayLength(function(keyLength) {
+          addPlayer(name, keyLength);
+        });
+
+        $('.rps-join').hide();
+      }
+
+    });
+
     function fillSeat(player) {
-      console.log(player);
       $('#p'+ player.id +'-waiting').hide();
       $('#p' + player.id + '-display').text(player.name);
       $('#p' + player.id + '-table').show();
     }
 
     function vacateSeat(playerId) {
-      $('#p'+ player.id +'-waiting').show();
-      $('#p' + player.id + '-display').text('');
-      $('#p' + player.id + '-table').hide();
+
+      var $container = $('#p' + playerId + '-wrapper').siblings('.rps-wrapper');
+
+      $container.find('.rps-waiting').show();
+      $container.find('.rps-display').text('');
+      $container.find('.table').hide();
+
     }
 
     function addPlayer(name, index) {
@@ -78,41 +89,26 @@
       });
     }
 
-    function getPlayerKeys(callback) {
+    function getFBArrayLength(callback) {
       playerDB.once('value', function(snapshot) {
 
         var len = 0;
+
         if(snapshot.val()) {
           len = Object.keys(snapshot.val()).length;
         }
 
         len++;
-
         callback(len);
 
       });
     }
-
-    $('.rps-join-game').on('click', function() {
-      console.log('hit kesdfsdys');
-      var name = $('#rps-name').val().trim();
-
-      if(name) {
-        getPlayerKeys(function(keyLength) {
-          console.log(keyLength);
-          addPlayer(name, keyLength);
-        });
-        $('.rps-join').hide();
-      }
-
-    });
 
     // When user leaves
     window.onbeforeunload = function () {
 
       var key = sessionStorage.getItem('key', key);
       playerDB.child(key).remove();
-      sessionStorage.clear();
 
     };
 
